4615fc87c3028105ea17fe9fcb70b3741737cbd4
red57 <red57@example.com>
2023-05-17 22:13:37 +0300

    fix client.py

--- client/client.py
+++ client/client.py
@@ -3,9 +3,9 @@ import urllib.request
 client = urllib.request.urlopen("http://127.0.0.1:9057/")
 
 # Декодирование ответа
-# encodedContent = client.read()
-# decodedContent = encodedContent.decode("utf8")
+encodedContent = client.read()
+decodedContent = encodedContent.decode("utf8")
 
-print("decodedContent")
+print(decodedContent)
 
 client.close()
